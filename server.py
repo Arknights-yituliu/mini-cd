602812ccd2939a18a96be27ef0562bfe8899baa8
Zhao Zuohong <zhbaor@zhaozuohong.vip>
2023-03-30 16:27:11 +0800

    部署服务

--- server.py
+++ server.py
@@ -3,6 +3,7 @@
 from bottle import post, run, request
 import urllib3
 import json
+from subprocess import Popen
 
 http = urllib3.PoolManager()
 
@@ -11,15 +12,25 @@ http = urllib3.PoolManager()
 def hook():
     req = request.json
     tag = req["push_data"]["tag"]
-    branch = "stable" if tag == "latest" else "dev"
+    if tag == "latest":
+        branch = "stable"
+        name = "v2plus-stable"
+        port = 10001
+        target_prefix = ""
+    else:  # tag = "dev"
+        branch = "dev"
+        name = "v2plus"
+        port = 10000
+        target_prefix = "dev."
     callback = req["callback_url"]
-    target_prefix = "dev." if tag == "dev" else ""
-    r = http.request("POST", callable, json.dumps({
+    http.request("POST", callable, json.dumps({
         "state": "success",
         "description": "",
         "context": f"Deploy {branch} branch",
         "target_url": f"https://{target_prefix}yituliu.site"
     }).encode("utf-8"))
+    Popen(
+        f"docker pull zhaozuohong/yituliu-frontend-v2-plus:{tag} && docker stop {name} && docker run -d --rm --name {name} -e PORT={port} -p {port}:{port} zhaozuohong/yituliu-frontend-v2-plus:{tag}", shell=True)
     return (f"callback: {callback}, branch: {branch}")
 
 
