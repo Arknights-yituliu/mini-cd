b92929bd90e05c7ce0255f52e035b9616a02ee21
Zhao Zuohong <zhbaor@zhaozuohong.vip>
2023-03-30 20:06:34 +0800

    只自动部署dev分支

--- server.py
+++ server.py
@@ -14,13 +14,9 @@ def hook():
     tag = req["push_data"]["tag"]
     if tag == "latest":
         branch = "stable"
-        name = "v2plus-stable"
-        port = 10001
         target_prefix = ""
     else:  # tag = "dev"
         branch = "dev"
-        name = "v2plus"
-        port = 10000
         target_prefix = "dev."
     callback = req["callback_url"]
     http.request("POST", callback,  body=json.dumps({
@@ -29,8 +25,13 @@ def hook():
         "context": f"Deploy {branch} branch",
         "target_url": f"https://{target_prefix}yituliu.site"
     }).encode("utf-8"), headers={'Content-Type': 'application/json'})
-    Popen(
-        f"docker pull zhaozuohong/yituliu-frontend-v2-plus:{tag} && docker stop {name} && docker run -d --rm --name {name} -e PORT={port} -p {port}:{port} zhaozuohong/yituliu-frontend-v2-plus:{tag}", shell=True)
+    if tag == "latest":
+        Popen(
+            "docker pull zhaozuohong/yituliu-frontend-v2-plus:latest", shell=True)
+    else:
+        Popen(
+            "docker pull zhaozuohong/yituliu-frontend-v2-plus:dev && docker stop v2plus && docker run -d --rm --name v2plus -e PORT=10000 -p 10000:10000 zhaozuohong/yituliu-frontend-v2-plus:dev", shell=True)
+
     return (f"callback: {callback}, branch: {branch}")
 
 
