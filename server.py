da3403b11469c93e59ba12c642a860df4835ce60
Zhao Zuohong <zhbaor@zhaozuohong.vip>
2023-04-05 12:33:20 +0800

    只部署dev分支，不管main

--- server.py
+++ server.py
@@ -12,27 +12,18 @@ http = urllib3.PoolManager()
 def hook():
     req = request.json
     tag = req["push_data"]["tag"]
-    if tag == "latest":
-        branch = "stable"
-        target_prefix = ""
-    else:  # tag = "dev"
-        branch = "dev"
-        target_prefix = "dev."
     callback = req["callback_url"]
     http.request("POST", callback,  body=json.dumps({
         "state": "success",
         "description": "",
-        "context": f"Deploy {branch} branch",
-        "target_url": f"https://{target_prefix}yituliu.site"
+        "context": "",
+        "target_url": ""
     }).encode("utf-8"), headers={'Content-Type': 'application/json'})
-    if tag == "latest":
-        Popen(
-            "docker pull zhaozuohong/yituliu-frontend-v2-plus:latest", shell=True)
-    else:
+    if tag == "dev":
         Popen(
             "docker pull zhaozuohong/yituliu-frontend-v2-plus:dev && docker stop v2plus && docker run -d --rm --name v2plus -e PORT=10000 -p 10000:10000 zhaozuohong/yituliu-frontend-v2-plus:dev", shell=True)
 
-    return (f"callback: {callback}, branch: {branch}")
+    return ("OK")
 
 
 run(host="0.0.0.0", port=11000)
