6dfe6eada6785da971ad8a18a7f8caa410f48122
Zhao Zuohong <zhbaor@zhaozuohong.vip>
2023-05-17 22:07:51 +0800

    不再请求回调URL

--- server.py
+++ server.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
 
 from bottle import post, run, request
-import urllib3
+# import urllib3
 import json
 from subprocess import Popen
 
-http = urllib3.PoolManager()
+# http = urllib3.PoolManager()
 
 
 @post("/hook")
 def hook():
     req = request.json
     tag = req["push_data"]["tag"]
-    callback = req["callback_url"]
-    http.request("POST", callback,  body=json.dumps({
-        "state": "success",
-        "description": "",
-        "context": "",
-        "target_url": ""
-    }).encode("utf-8"), headers={'Content-Type': 'application/json'})
+    # callback = req["callback_url"]
+    # http.request("POST", callback,  body=json.dumps({
+    #     "state": "success",
+    #     "description": "",
+    #     "context": "",
+    #     "target_url": ""
+    # }).encode("utf-8"), headers={'Content-Type': 'application/json'})
     if tag == "dev":
         Popen(
             "docker pull zhaozuohong/yituliu-frontend-v2-plus:dev && docker rm -f v2plus && docker run -d --rm --name v2plus -e PORT=10000 -p 10000:10000 zhaozuohong/yituliu-frontend-v2-plus:dev", shell=True)
